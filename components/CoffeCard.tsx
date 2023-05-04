cebe2addb996f359e2e1724d1f1dc25ffd87cae4
Thiago <ps-thiago@hotmail.com>
2023-05-03 23:50:57 -0300

    feat coffeeCard home with cartContext

--- components/CoffeCard.tsx
+++ components/CoffeCard.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import { Minus, Plus } from 'phosphor-react'
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { CartContext } from '../contexts/CartContext'
 import ShoppCartWhite from '../public/cartwhite.svg'
 
-interface Product {
+export interface Product {
   id: number
   span: string[]
   name: string
@@ -17,11 +18,33 @@ interface ProductProps {
 }
 
 export const CoffeCard = ({ product }: ProductProps) => {
+  const [quantity, setQuantity] = useState(1)
+
+  function handleMoreProductQuantity() {
+    setQuantity((state) => state + 1)
+  }
+
+  function handleLessProductQuantity() {
+    setQuantity((state) => state - 1)
+  }
+
+  const { addNewProductToCart } = useContext(CartContext)
+
+  function handleAddProductToCart() {
+    const addTheProduct = {
+      ...product,
+      quantity
+    }
+    setQuantity((state) => 1)
+    addNewProductToCart(addTheProduct)
+  }
+
   return (
     <div className=" bg-base-card rounded-md rounded-tr-[36px] rounded-bl-[36px] mb-10">
       <div className="flex justify-center ">
         <div className="mt-[-1.25rem]">
           <Image
+            alt=""
             src={`/coffesImgs/${product.image}`}
             width={120}
             height={120}
@@ -32,7 +55,7 @@ export const CoffeCard = ({ product }: ProductProps) => {
       <div className="flex items-center justify-center mt-3">
         {product.span.map((span) => (
           <span
-            key={product.id}
+            key={`${product.id}.${span}`}
             className="px-2 py-1 mx-1 text-yellow bg-yellow-light rounded-full text-[10px]"
           >
             {span}
@@ -53,17 +76,27 @@ export const CoffeCard = ({ product }: ProductProps) => {
           9,90
         </span>
         <div className="ml-[23px] mr-2 h-full bg-base-button gap-2 rounded-md whitespace-nowrap">
-          <button className="ml-2 mr-1 my-3">
+          <button
+            className="ml-2 mr-1 my-3"
+            onClick={handleLessProductQuantity}
+            disabled={quantity <= 1}
+          >
             <Minus size={14} color="#8047F8" />
           </button>
-          <span>1</span>
-          <button className="ml-1 mr-2 my-3 ">
+          <span>{quantity}</span>
+          <button
+            className="ml-1 mr-2 my-3 "
+            onClick={handleMoreProductQuantity}
+          >
             <Plus size={14} color="#8047F8" />
           </button>
         </div>
 
-        <button className=" bg-purple-dark flex items-center justify-center ml-2 w-full rounded-md h-full">
-          <Image src={ShoppCartWhite} width={22} height={22} />
+        <button
+          className=" bg-purple-dark flex items-center justify-center ml-2 w-full rounded-md h-full"
+          onClick={handleAddProductToCart}
+        >
+          <Image alt="" src={ShoppCartWhite} width={22} height={22} />
         </button>
       </div>
     </div>
