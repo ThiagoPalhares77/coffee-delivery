1d2ec816b557161cb1cdc003e5df5a6f078ab045
Thiago <ps-thiago@hotmail.com>
2023-05-03 23:43:32 -0300

    feat CartContexProvider in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { Header } from '../components/Header'
+import { CartContextProvider } from '../contexts/CartContext'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className="max-w-[1120px] mx-auto">
-      <Header />
-      <Component {...pageProps} />
+      <CartContextProvider>
+        <Header />
+        <Component {...pageProps} />
+      </CartContextProvider>
     </div>
   )
 }
